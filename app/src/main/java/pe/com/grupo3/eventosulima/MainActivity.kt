3ea014de1f664dd94f177c54d47f3f4817b3da97
DogeFox071101 <lgfalconch@hotmail.com>
2022-07-06 19:56:24 -0500

    Commit Inicial

---
+++ app/src/main/java/pe/com/grupo3/eventosulima/MainActivity.kt
@@ -0,0 +1,11 @@
+package pe.com.grupo3.eventosulima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
