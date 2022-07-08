9e68a440d10b17e6d608860868caba5de3542a36
bachmaster <brunoandrechirinosmolina@gmail.com>
2022-07-08 17:51:17 -0500

    Se agrega la primera pantalla, la pantalla de LoginActivity.kt

---
+++ app/src/main/java/pe/com/grupo3/eventosulima/LoginActivity.kt
@@ -0,0 +1,12 @@
+package pe.com.grupo3.eventosulima
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import pe.com.grupo3.eventosulima.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
