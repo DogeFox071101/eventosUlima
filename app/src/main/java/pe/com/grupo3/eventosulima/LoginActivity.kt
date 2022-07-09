e1ac3be5c5064b1772600f78661f77abb2d73929
bachmaster <brunoandrechirinosmolina@gmail.com>
2022-07-08 22:28:15 -0500

    Se agrega la primera pantalla, la pantalla de RegisterActivity.kt

--- app/src/main/java/pe/com/grupo3/eventosulima/LoginActivity.kt
+++ app/src/main/java/pe/com/grupo3/eventosulima/LoginActivity.kt
@@ -1,12 +1,20 @@
 package pe.com.grupo3.eventosulima
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import pe.com.grupo3.eventosulima.R
 
 class LoginActivity: AppCompatActivity() {
+    lateinit var  btnRegistrarse: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnRegistrarse = findViewById(R.id.btnRegistrarse)
+        btnRegistrarse.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/pe/com/grupo3/eventosulima/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package pe.com.grupo3.eventosulima
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+    }
+}
