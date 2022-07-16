9d2113794c5502a543a440bf60c8fa2316d59656
bachmaster <brunoandrechirinosmolina@gmail.com>
2022-07-15 19:46:28 -0500

    Se ha creado la pantalla butacas, recien se le ha terminado de hacer el dropdown menu y ahora se está implementando la matriz de butacas (en proceso)

--- app/src/main/java/pe/com/grupo3/eventosulima/MainActivity.kt
+++ app/src/main/java/pe/com/grupo3/eventosulima/MainActivity.kt
@@ -2,10 +2,12 @@ package pe.com.grupo3.eventosulima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
