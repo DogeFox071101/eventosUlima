c3628f2f8f076563038aaa588b31c64af4b6cd26
DogeFox071101 <lgfalconch@hotmail.com>
2022-07-16 00:51:41 -0500

    algo v4

--- app/src/main/java/pe/com/grupo3/eventosulima/MainActivity.kt
+++ app/src/main/java/pe/com/grupo3/eventosulima/MainActivity.kt
@@ -3,11 +3,13 @@ package pe.com.grupo3.eventosulima
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        MobileAds.initialize(this) {}
     }
 }
