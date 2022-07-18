e59da3d8f01c36aa6ac09378c7091758bd84d8c1
bachmaster <brunoandrechirinosmolina@gmail.com>
2022-07-18 10:45:08 -0500

    tratando de mostrar los anuncios de google services

--- app/src/main/java/pe/com/grupo3/eventosulima/fragments/ListaEventosFragment.kt
+++ app/src/main/java/pe/com/grupo3/eventosulima/fragments/ListaEventosFragment.kt
@@ -9,6 +9,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -19,9 +21,14 @@ import pe.com.grupo3.eventosulima.adapters.ListadoEventosAdapter
 import pe.com.grupo3.eventosulima.models.GestorEventos
 import pe.com.grupo3.eventosulima.models.beans.Evento
 
+
 class ListaEventosFragment : Fragment() {
 
     private lateinit var mrviEventos : RecyclerView
+
+    private val TAG = "MainActivity"
+
+    private var mAdView: AdView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,6 +74,12 @@ class ListaEventosFragment : Fragment() {
                 }
             }
         }
+
+
+        mAdView = view.findViewById(R.id.adView)
+        val adRequest: AdRequest = AdRequest.Builder().build()
+        mAdView?.loadAd(adRequest)
+
     }
 
     private fun cargarListaEventos(lista: List<Evento>) {
